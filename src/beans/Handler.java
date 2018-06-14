1c4321ef0d1eb9393d22c336adc2d79689852678
TonyTulpe <TonyTulpe@TonyTulpe-PC.localdomain>
2018-06-14 17:35:13 +0200

    Fehlende Dateien

--- src/beans/Handler.java
+++ src/beans/Handler.java
@@ -6,17 +6,37 @@ import java.util.List;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+import bestellvorgang.Kunde;
 import datenbank.Buch;
 import de.imut.ec.keyvaluestore.KeyValueStore;
 
 abstract class Handler {
 	
-	private String key = "lBenYS9JqrKN2ld8dlkmICXiEVmYQPaIWDKid762";
-	private KeyValueStore kvs = new KeyValueStore();
+	protected String key_buecher = "lBenYS9JqrKN2ld8dlkmICXiEVmYQPaIWDKid762";
+	protected String key_kunde = "8mVgu735LzJqtNG9FQMuDKASu0VowPoX5c6SfjQk";
 	
-	private Gson json = new Gson();
+	protected KeyValueStore kvs = new KeyValueStore();
+	
+	protected Gson json = new Gson();
 	private TypeToken<ArrayList<Buch>> token_buecher = new TypeToken<ArrayList<Buch>>() {};
+	private TypeToken<ArrayList<Kunde>> token_kunde = new TypeToken<ArrayList<Kunde>>() {};
 	
-	protected List<Buch> buecher = new ArrayList<Buch>(json.fromJson(kvs.get(key), token_buecher.getType()));
-
+	protected List<Buch> buecher;
+	protected List<Kunde> kunden;
+	
+	protected void init() {
+		
+		try {
+			this.buecher = new ArrayList<Buch>(json.fromJson(kvs.get(key_buecher), token_buecher.getType()));
+			this.kunden = new ArrayList<Kunde>(json.fromJson(kvs.get(key_kunde), token_kunde.getType()));
+		}catch(NullPointerException e) {
+			if(this.kunden == null) {
+				this.kunden = new ArrayList<Kunde>();
+			}
+			else if(this.buecher == null) {
+				this.buecher = new ArrayList<Buch>();
+			}
+		}
+		
+	}
 }
---
+++ src/beans/KundenHandler.java
@@ -0,0 +1,42 @@
+package beans;
+
+import java.util.ArrayList;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
+import bestellvorgang.Kunde;
+
+@ManagedBean(name="kHandler")
+@SessionScoped
+public class KundenHandler extends Handler{
+
+	private Kunde kunde;
+	
+	public KundenHandler() {
+		this.kunde = new Kunde();
+	}
+	
+	public Kunde getKunde() {
+		return this.kunde;
+	}
+	
+	public String save() {
+		
+		if(this.kunden == null) {
+			this.kunden = new ArrayList<Kunde>();
+		}
+		
+		HttpSession fc = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(true);
+		
+		this.kunde.setSessionId(fc.getId());
+		
+		this.kunden.add(this.kunde);
+		this.kvs.put(this.key_kunde, this.json.toJson(this.kunden));
+		
+		return "kontaktformular.xhtml";
+	}
+	
+}
